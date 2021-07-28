b503c6ce8d6520541bec517fd8d33122b690f6fc
ahernandezr05 <datw.alberto@gmail.com>
2021-07-28 17:03:38 +0200

    0.1.001 Final

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,24 @@
 function geekshubs(index) {
-    return "";
+    var iterador;
+    iterador = index
+    for(i=1; i<=iterador; i++){
+        if(i%3 == 0 && i%5 == 0){
+            console.log("GeeksHubs");
+        }else{
+            if(i%5 == 0){
+                console.log("Hubs");
+            }else{
+                if(i%3 == 0){
+                    console.log("Geeks");
+                }else{
+                console.log(i)
+            }
+        }
+
+        }
+    }
+
 }
+
+
 module.exports = geekshubs;
