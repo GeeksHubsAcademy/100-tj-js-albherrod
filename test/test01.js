6071c7a3932146d81a46ee5e73bd6caf6ca13a5b
ahernandezr05 <datw.alberto@gmail.com>
2021-07-28 18:52:37 +0200

    Tiempo Aprox 30, intentos 2

--- test/test01.js
+++ test/test01.js
@@ -1,23 +1,28 @@
 function geekshubs(index) {
     var iterador;
+    var print = "";
     iterador = index
     for(i=1; i<=iterador; i++){
         if(i%3 == 0 && i%5 == 0){
             console.log("GeeksHubs");
+            print = print + "GeeksHubs \n";
         }else{
             if(i%5 == 0){
                 console.log("Hubs");
+                print = print + "Hubs \n";
             }else{
                 if(i%3 == 0){
                     console.log("Geeks");
+                    print = print + "Geeks\n";
                 }else{
                 console.log(i)
+                print = print + i + "\n";
             }
         }
 
         }
     }
-
+    return print;
 }
 
 module.exports = geekshubs;
